d4b01e09ee99b202805977aac2cd1caeb556b543
Arnau Abella <arnauabella@gmail.com>
2023-04-08 09:51:30 +0200

    Refactor to simplify the code

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,23 @@ use std::collections::HashSet;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, parse_quote, DataEnum, DataStruct, DeriveInput};
+use syn::{
+    parse_macro_input, parse_quote, spanned::Spanned, Data, DataEnum, DataStruct, DeriveInput,
+    Error, Fields, Generics, Ident, Type, TypePath, Variant,
+};
+
+const DEFAULT_VARIANT_KEYWORD: &str = "default";
 
 #[proc_macro_derive(Default, attributes(default))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let output = match __derive(parse_macro_input!(input as DeriveInput)) {
         Ok(output) => output,
-        Err(err) => err.into_compile_error().into(),
+        Err(err) => err.into_compile_error(),
     };
     proc_macro::TokenStream::from(output)
 }
 
-fn __derive(input: DeriveInput) -> Result<proc_macro2::TokenStream, syn::Error> {
+fn __derive(input: DeriveInput) -> Result<proc_macro2::TokenStream, Error> {
     let DeriveInput {
         attrs: _,
         vis: _,
@@ -23,11 +28,11 @@ fn __derive(input: DeriveInput) -> Result<proc_macro2::TokenStream, syn::Error> 
     } = input;
 
     let (body, fields) = match data {
-        syn::Data::Struct(data) => struct_case(&input_ident, data),
-        syn::Data::Enum(data) => enum_case(&input_ident, data),
-        syn::Data::Union(_) => Err(syn::Error::new_spanned(
+        Data::Struct(data) => struct_case(&input_ident, data),
+        Data::Enum(data) => enum_case(&input_ident, data),
+        Data::Union(_) => Err(Error::new_spanned(
             &input_ident,
-            "#[derive(Default)] is only supported for unions",
+            "#[derive(Default)] is not supported for unions",
         )),
     }?;
 
@@ -46,105 +51,88 @@ fn __derive(input: DeriveInput) -> Result<proc_macro2::TokenStream, syn::Error> 
 }
 
 fn struct_case(
-    struct_ident: &syn::Ident,
+    struct_ident: &Ident,
     data: DataStruct,
-) -> Result<(proc_macro2::TokenStream, syn::Fields), syn::Error> {
-    let constr = match data.fields.clone() {
-        syn::Fields::Unit => quote!(#struct_ident),
-        syn::Fields::Unnamed(unnamed) => {
-            let fields_constr = unnamed.unnamed.into_iter().map(|field| {
-                let ty = field.ty;
-                quote!(#ty::default())
-            });
-            quote!(#struct_ident(#(#fields_constr),*))
-        }
-        syn::Fields::Named(named) => {
-            let fields_constr = named.named.into_iter().map(|field| {
-                let field_name = field.ident.expect("named fields should contain an ident");
-                let ty = &field.ty;
-                quote!(#field_name : #ty::default())
-            });
+) -> Result<(proc_macro2::TokenStream, Fields), Error> {
+    let data_constr = default_instance_constr(struct_ident, &data.fields);
 
-            quote! {
-                #struct_ident{#(#fields_constr),*}
-            }
-        }
-    };
-
-    Ok((constr, data.fields))
+    Ok((data_constr, data.fields))
 }
 
 fn enum_case(
-    root_ident: &syn::Ident,
+    root_ident: &Ident,
     data: DataEnum,
-) -> Result<(proc_macro2::TokenStream, syn::Fields), syn::Error> {
+) -> Result<(proc_macro2::TokenStream, Fields), Error> {
     if data.variants.is_empty() {
-        return Err(syn::Error::new_spanned(
+        return Err(Error::new_spanned(
             root_ident,
             "#[derive(Default)] is not supported for empty enums",
         ));
     }
 
-    let mut default_variants = data
-        .variants
-        .into_iter()
-        .filter(|variant| has_default_attr(variant).unwrap_or_default());
+    let mut default_variants = data.variants.into_iter().filter(has_default_attr);
 
-    if let Some(default_variant) = default_variants.next() {
-        if let Some(another_default_variant) = default_variants.next() {
-            return Err(syn::Error::new_spanned(
-                another_default_variant,
-                "#[default] is defined more than once",
-            ));
+    match (default_variants.next(), default_variants.next()) {
+        (Some(default_variant), None) => {
+            let default_variant_constr = {
+                // Something as below would be great, but `Self::XXX` is not a valid identifier
+                // let variant_ident = Ident::new(&format!("Self::{}", &default_variant.ident), Span::call_site());
+                let constr =
+                    default_instance_constr(&default_variant.ident, &default_variant.fields);
+                quote!(Self::#constr)
+            };
+
+            Ok((default_variant_constr, default_variant.fields))
         }
 
-        let variant_ident = default_variant.ident.clone();
-        let default_variant_constr = match default_variant.fields.clone() {
-            syn::Fields::Unit => {
-                quote! {
-                    Self::#variant_ident
-                }
-            }
-            syn::Fields::Unnamed(unnamed) => {
-                let fields_constr = unnamed.unnamed.into_iter().map(|field| {
-                    let ty = field.ty;
-                    quote!(#ty::default())
-                });
-
-                quote! {
-                    Self::#variant_ident(#(#fields_constr),*)
-                }
-            }
-            syn::Fields::Named(named) => {
-                let fields_constr = named.named.into_iter().map(|field| {
-                    let field_name = field.ident.expect("named fields should contain an ident");
-                    let ty = &field.ty;
-                    quote!(#field_name : #ty::default())
-                });
-
-                quote! {
-                    Self::#variant_ident{#(#fields_constr),*}
-                }
-            }
-        };
-
-        Ok((default_variant_constr, default_variant.fields))
-    } else {
-        Err(syn::Error::new_spanned(
+        (Some(default_variant), Some(another_default_variant)) => {
+            let span = another_default_variant
+                .span()
+                .join(default_variant.span())
+                .expect("self and other ARE from the same file");
+            Err(Error::new(span, "#[default] is defined multiple times"))
+        }
+        (None, _) => Err(Error::new_spanned(
             root_ident,
             "expected one variant with #[default]",
-        ))
+        )),
     }
 }
 
-fn has_default_attr(variant: &syn::Variant) -> Option<bool> {
-    let attr = variant.attrs.get(0)?;
-    let is_default = attr.path().is_ident("default");
-    Some(is_default)
+fn default_instance_constr(data_constr_ident: &Ident, fields: &Fields) -> proc_macro2::TokenStream {
+    match fields {
+        Fields::Unit => quote!(#data_constr_ident),
+        Fields::Unnamed(unnamed) => {
+            let fields_constr = unnamed.unnamed.iter().map(|field| {
+                let ty = &field.ty;
+                quote!(#ty::default())
+            });
+            quote!(#data_constr_ident(#(#fields_constr),*))
+        }
+        Fields::Named(named) => {
+            let fields_constr = named.named.iter().map(|field| {
+                let field_name = field
+                    .ident
+                    .as_ref()
+                    .expect("named fields should contain an ident");
+                let ty = &field.ty;
+                quote!(#field_name : #ty::default())
+            });
+            quote!(#data_constr_ident{#(#fields_constr),*})
+        }
+    }
 }
 
-fn add_trait_bounds(generics: &mut syn::Generics, fields: &syn::Fields) {
-    let used_types: HashSet<syn::Ident> = fields
+fn has_default_attr(variant: &Variant) -> bool {
+    variant
+        .attrs
+        .get(0)
+        .map(|attr| attr.path().is_ident(DEFAULT_VARIANT_KEYWORD))
+        .unwrap_or_default()
+}
+
+fn add_trait_bounds(generics: &mut Generics, fields: &Fields) {
+    let used_types: HashSet<Ident> = fields
         .iter()
         .filter_map(|field| type_ident(&field.ty))
         .cloned()
@@ -159,8 +147,8 @@ fn add_trait_bounds(generics: &mut syn::Generics, fields: &syn::Fields) {
     }
 }
 
-fn type_ident(ty: &syn::Type) -> Option<&syn::Ident> {
-    if let &syn::Type::Path(syn::TypePath {
+fn type_ident(ty: &Type) -> Option<&Ident> {
+    if let &Type::Path(TypePath {
         qself: None,
         ref path,
     }) = ty
